b9ee72692cefe38beb02df5dc8ff588e696714d9
devalidad <me@alidad.dev>
2023-03-01 00:07:46 -0800

    added cors and data.sql for UI

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into `Product` (`name`, `price`, `quantity`) values
+                                                ('apple', 1.50, 10),
+                                                ('cookies', 2.50, 10),
+                                                ('gum', 3.50, 11);
+
+insert into `Recalled_Product` (`name`) values ('gum');
